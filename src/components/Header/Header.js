6e45e0b9fb2d919a565db3b9ca533799dab22bfc
Robert Kibet <kipronokrobert@gmail.com>
2022-06-16 11:52:45 +0300

    feat: test findby, queryby, getby and getallby

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
-export default function Header({
-    title
-}) {
-    return <h1 className="header">{title}</h1>
+export default function Header({title}) {
+  return (
+    <>
+      <h1 className="header" title="Header" data-testid="header-title">
+        {title}
+      </h1>
+      <h3 className="header">Dogs</h3>
+    </>
+  );
 }
