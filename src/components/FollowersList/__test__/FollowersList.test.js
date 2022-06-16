82d9870e6893065bd9a31e2128bfc584a892492e
Robert Kibet <kipronokrobert@gmail.com>
2022-06-16 13:33:06 +0300

    http requests: test async calls to an api and data received

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,27 @@
+import {render, screen} from "@testing-library/react";
+import FollowersList from "../FollowersList";
+import {BrowserRouter} from "react-router-dom";
+
+describe("FollowersList", () => {
+  it("renders followers list and we can get the first card", async () => {
+    render(
+      <BrowserRouter>
+        <FollowersList />
+      </BrowserRouter>
+    );
+
+    const followerDiv = await screen.findByTestId("follower-item-0");
+    expect(followerDiv).toBeInTheDocument();
+  });
+
+  it("renders multiple followers", async () => {
+    render(
+      <BrowserRouter>
+        <FollowersList />
+      </BrowserRouter>
+    );
+
+    const followerDivs = await screen.findAllByTestId(/follower-item/);
+    expect(followerDivs.length).toBeGreaterThan(2);
+  });
+});
